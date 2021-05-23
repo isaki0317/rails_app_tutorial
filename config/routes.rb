74e0d313ff52e6d53df87a2b6b544e03868dd274
isaki0317 <matsuoisaki.0317@gmail.com>
2021-05-23 22:02:05 +0900

    feature: password_reset_mailer / next: 12.2.2

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   post '/signup',  to: 'users#create'
 
   resources :account_activations, only: [:edit]
+  resources :password_resets, only: [:new, :create, :edit, :update]
   resources :users
 
   root to: 'static_pages#home'
