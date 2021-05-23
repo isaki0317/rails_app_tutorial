759b56397ce812616837a738f54b8020ae67cc71
isaki0317 <matsuoisaki.0317@gmail.com>
2021-05-23 16:29:22 +0900

    feature: add account_activations_controller

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
 
   get '/signup', to: 'users#new'
   post '/signup',  to: 'users#create'
+
+  resources :account_activations, only: [:edit]
   resources :users
 
   root to: 'static_pages#home'
