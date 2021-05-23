2d641978a39f764a22cc199785b9a5712aa60c60
isaki0317 <matsuoisaki.0317@gmail.com>
2021-05-23 20:13:46 +0900

    refactor: n+1 test: signup test

--- app/models/user.rb
+++ app/models/user.rb
@@ -43,6 +43,14 @@ class User < ApplicationRecord
     update_attribute(:remember_digest, nil)
   end
 
+  def activate
+    update_columns(activated: true, activated_at: Time.zone.now)
+  end
+
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+  end
+
   private
 
     # emailを小文字に統一して保存
